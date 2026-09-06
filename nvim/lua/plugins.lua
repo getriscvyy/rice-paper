c08fa4846c19ce529117f8ff3e7e1775bc2527bb
Alex Sun <alexsun0725@gmail.com>
2026-09-05 23:31:05 -0500

    feat: add nvim tweaks

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -7,5 +7,32 @@ vim.pack.add({
   "https://github.com/MunifTanjim/nui.nvim",
   "https://github.com/nvim-tree/nvim-web-devicons",
   "https://github.com/lervag/vimtex",
+  "https://github.com/catppuccin/nvim",
 })
+
+require('neo-tree').setup {
+  filesystem = {
+    filtered_items = {
+      visible = true,
+      hide_dotfiles = false,
+      hide_gitignored = true,
+    },
+  },
+}
+
+require('catppuccin').setup {
+  flavour = 'mocha',
+  transparent_background = true,
+  integrations = {
+    neotree = true,
+  },
+}
+
+vim.cmd.colorscheme('catppuccin')
+
+local mauve = '#cba6f7' -- mocha; swap per your flavour
+
+vim.api.nvim_set_hl(0, 'NeoTreeDirectoryName', { fg = mauve })
+vim.api.nvim_set_hl(0, 'NeoTreeDirectoryIcon', { fg = mauve })
+vim.api.nvim_set_hl(0, 'NeoTreeRootName', { fg = mauve, bold = true })
 
